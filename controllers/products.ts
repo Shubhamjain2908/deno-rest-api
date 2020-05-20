7bd3f10955ac7024be44600c4504c68023117705
Shubham Jain <Shubhamjain2908@gmail.com>
2020-05-21 02:33:05 +0530

    delete product

--- controllers/products.ts
+++ controllers/products.ts
@@ -130,8 +130,14 @@ const updateProduct = async (
  * @route DELETE /api/v1/products/:id
  * @param response
  */
-const deleteProduct = ({ response }: { response: any }) => {
-  response.body = "delete";
+const deleteProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  products = products.filter((p) => p.id !== params.id);
+  response.body = {
+    success: true,
+    msg: "Product removed",
+  };
 };
 
 export { getProducts, getProduct, addProduct, updateProduct, deleteProduct };
