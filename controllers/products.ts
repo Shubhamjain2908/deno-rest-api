bc3cc6b635801a8e761311f949a19da92b3eb827
Shubham Jain <Shubhamjain2908@gmail.com>
2020-05-21 02:27:53 +0530

    add product

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,3 +1,4 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import { Product } from "../types.ts";
 
 let products: Product[] = [
@@ -63,8 +64,27 @@ const getProduct = (
  * @route POST /api/v1/products
  * @param response
  */
-const addProduct = ({ response }: { response: any }) => {
-  response.body = "add";
+const addProduct = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
+
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      msg: "No data",
+    };
+  } else {
+    const product: Product = body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
 };
 
 /**
