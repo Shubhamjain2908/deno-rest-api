786342836ce1a54e9718781a7dc4dc7cae6ac88c
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-05 02:56:42 +0530

    db added in add product

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,5 +1,8 @@
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+import { dbCreds } from "../config.ts";
 import { Product } from "../types.ts";
+
+const client = new Client(dbCreds);
 
 let products: Product[] = [
   {
@@ -68,6 +71,7 @@ const addProduct = async (
   { request, response }: { request: any; response: any },
 ) => {
   const body = await request.body();
+  const product = body.value;
 
   if (!request.hasBody) {
     response.status = 400;
@@ -76,14 +80,29 @@ const addProduct = async (
       msg: "No data",
     };
   } else {
-    const product: Product = body.value;
-    product.id = v4.generate();
-    products.push(product);
-    response.status = 201;
-    response.body = {
-      success: true,
-      data: product,
-    };
+    try {
+      await client.connect();
+      const result = await client.query(
+        "INSERT INTO products(name, description, price) VALUES($1, $2, $3)",
+        product.name,
+        product.description,
+        product.price,
+      );
+
+      response.status = 201;
+      response.body = {
+        success: true,
+        data: product,
+      };
+    } catch (error) {
+      response.status = 500;
+      response.body = {
+        success: false,
+        data: error.toString(),
+      };
+    } finally {
+      await client.end();
+    }
   }
 };
 
